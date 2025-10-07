09a0e4e5ce207cf15c02596434fa81f15c58cb9c
ryanfavour4 <ryanfave022@gmail.com>
2025-10-07 12:35:23 +0100

    `Refactored Footer and Hero components with minor changes to links and layout`

--- src/component/Footer.tsx
+++ src/component/Footer.tsx
@@ -6,95 +6,95 @@ import FaceBookIcon from "../svg/facebook";
 import AllRightReserved from "./AllRightRESER";
 
 type Link = {
-  id: number;
-  name: string;
-  link: string;
+    id: number;
+    name: string;
+    link: string;
 };
 
 type Links = Link[];
 
 const Links: Links = [
-  { id: 1, name: "Home", link: "/" },
-  { id: 2, name: "About", link: "/#about" },
-  { id: 3, name: "Contact", link: "/#contact" },
-  { id: 4, name: "FAQs", link: "/#FAQs" },
+    { id: 1, name: "Home", link: "/" },
+    { id: 3, name: "Contact", link: "/#contact" },
+    { id: 4, name: "FAQs", link: "/#FAQs" },
 ];
 
 const Legals: Links = [
-  { id: 1, name: "Terms of Use", link: "/#termsOfUse" },
-  { id: 2, name: "Help Center", link: "/#helpCenter" },
-  { id: 3, name: "Privacy Policy", link: "/#privacyPolicy" },
-  { id: 4, name: "Company", link: "/#company" },
+    { id: 1, name: "Terms of Use", link: "/#termsOfUse" },
+    { id: 3, name: "Privacy Policy", link: "/#privacyPolicy" },
+    { id: 4, name: "Company", link: "/#company" },
 ];
-// container mx-auto w-full
+
 export default function Footer() {
-  return (
-    <div className="pt-20">
-      <div className="container mx-auto w-full flex flex-col md:flex-row justify-between gap-10 md:gap-20 py-8 px-6">
-        {/* Left section */}
-        <div className="flex flex-col ">
-          <a href="/" className="w-fit">
-            <img
-              src={BillBoard}
-              alt="BillBoard"
-              className="w-44 md:w-56"
-              width={200}
-              height={200}
-            />
-          </a>
-          <p className="pb-10 pt-5 text-lg text-gray-700">
-            Smart banking for everyone.
-          </p>
+    return (
+        <div className="pt-20">
+            <div className="container mx-auto w-full flex flex-col md:flex-row justify-between gap-10 md:gap-20 py-8 px-6">
+                {/* Left section */}
+                <div className="flex flex-col ">
+                    <a href="/" className="w-fit">
+                        <img
+                            src={BillBoard}
+                            alt="BillBoard"
+                            className="w-44 md:w-56"
+                            width={200}
+                            height={200}
+                        />
+                    </a>
+                    <p className="pb-10 pt-5 text-lg text-gray-700">
+                        Smart banking for everyone.
+                    </p>
 
-          {/* Social icons */}
-          <div className="flex gap-4 mt-10">
-            <a href="#">
-              <FaceBookIcon className="w-6 h-6 hover:text-[#F4450B]" />
-            </a>
-            <a href="#">
-              <InstagramIcon className="w-6 h-6 hover:text-[#F4450B]" />
-            </a>
-            <a href="#">
-              <LinkinIcon className="w-6 h-6 hover:text-[#F4450B]" />
-            </a>
-            <a href="#">
-              <TwitterIcon className="w-6 h-6 hover:text-[#F4450B]" />
-            </a>
-          </div>
+                    {/* Social icons */}
+                    <div className="flex gap-4 mt-10">
+                        <a href="#">
+                            <FaceBookIcon className="w-6 h-6 hover:text-[#F4450B]" />
+                        </a>
+                        <a href="https://www.instagram.com/billboardapp.ng">
+                            <InstagramIcon className="w-6 h-6 hover:text-[#F4450B]" />
+                        </a>
+                        <a href="#">
+                            <LinkinIcon className="w-6 h-6 hover:text-[#F4450B]" />
+                        </a>
+                        <a href="https://x.com/billboardapp_ng">
+                            <TwitterIcon className="w-6 h-6 hover:text-[#F4450B]" />
+                        </a>
+                    </div>
+                </div>
+
+                {/* Right section */}
+                <div className="flex flex-col sm:flex-row justify-between gap-10">
+                    {/* Quick Links */}
+                    <div className="flex flex-col space-y-2">
+                        <h1 className="text-xl font-semibold mb-3">
+                            Quick Links
+                        </h1>
+                        {Links.map((link) => (
+                            <a
+                                key={link.id}
+                                href={link.link}
+                                className="hover:text-[#F4450B] transition-colors"
+                            >
+                                {link.name}
+                            </a>
+                        ))}
+                    </div>
+
+                    {/* Legal */}
+                    <div className="flex flex-col space-y-2">
+                        <h1 className="text-xl font-semibold mb-3">Legal</h1>
+                        {Legals.map((legal) => (
+                            <a
+                                key={legal.id}
+                                href={legal.link}
+                                className="hover:text-[#F4450B] transition-colors"
+                            >
+                                {legal.name}
+                            </a>
+                        ))}
+                    </div>
+                </div>
+            </div>
+            <AllRightReserved />
         </div>
-
-        {/* Right section */}
-        <div className="flex flex-col sm:flex-row justify-between gap-10">
-          {/* Quick Links */}
-          <div className="flex flex-col space-y-2">
-            <h1 className="text-xl font-semibold mb-3">Quick Links</h1>
-            {Links.map((link) => (
-              <a
-                key={link.id}
-                href={link.link}
-                className="hover:text-[#F4450B] transition-colors"
-              >
-                {link.name}
-              </a>
-            ))}
-          </div>
-
-          {/* Legal */}
-          <div className="flex flex-col space-y-2">
-            <h1 className="text-xl font-semibold mb-3">Legal</h1>
-            {Legals.map((legal) => (
-              <a
-                key={legal.id}
-                href={legal.link}
-                className="hover:text-[#F4450B] transition-colors"
-              >
-                {legal.name}
-              </a>
-            ))}
-          </div>
-        </div>
-      </div>
-      <AllRightReserved />
-    </div>
-  );
+    );
 }
--- src/component/Hero.tsx
+++ src/component/Hero.tsx
@@ -4,45 +4,56 @@ import styles from "../page.module.css";
 import ArrowRightUp from "../../public/Arrow right up.png";
 
 export default function Hero() {
-  return (
-    <div className="flex md:flex-row flex-col items-center w-full gap-5 pt-20">
-      {/* text */}
-      <div className="flex-col flex-1 ml-10">
-        <h1 className="text-6xl font-bold mb-6 leading-[1.2]">
-          Your Digital Bank for Every Moment.
-        </h1>
-        <p className=" max-w-[500px] text-justify text-base">
-          From your morning coffee to your biggest goals, our app is built to
-          move with you. Enjoy instant access to payments, savings, and rewards
-          in one smooth experience. We make sure your money works as hard as you
-          do — smarter, faster, and always secure.
-        </p>
-        {/* button */}
-        <div className="flex mt-8 gap-5">
-          <button className="bg-[#F4450B]  text-white px-8 py-3 rounded-full flex items-center gap-2 cursor-pointer">
-            Download
-            <span>
-              <img src={ArrowDown} alt="Arrow Down" width={15} height={15} />
-            </span>
-          </button>
-          <button className=" text-[#F4450B] px-8 py-3 rounded-full flex items-center border-2 gap-2 cursor-pointer">
-            Explore More
-            <span>
-              <img src={ArrowRightUp} alt="Arrow Down" width={15} height={15} />
-            </span>
-          </button>
+    return (
+        <div className="flex md:flex-row flex-col items-center w-full gap-5 pt-20">
+            {/* text */}
+            <div className="flex-col flex-1 md:ml-10 px-4">
+                <h1 className="text-5xl text-balance md:text-6xl font-bold mb-6 leading-[1.2]">
+                    Your Digital Bank for Every Moment.
+                </h1>
+                <p className=" max-w-[500px] text-justify text-base">
+                    From your morning coffee to your biggest goals, our app is
+                    built to move with you. Enjoy instant access to payments,
+                    savings, and rewards in one smooth experience. We make sure
+                    your money works as hard as you do — smarter, faster, and
+                    always secure.
+                </p>
+                {/* button */}
+                <div className="flex mt-8 gap-5">
+                    <button className="bg-[#F4450B]  text-white px-8 py-3 rounded-full flex items-center gap-2 cursor-pointer">
+                        Download
+                        <span>
+                            <img
+                                src={ArrowDown}
+                                alt="Arrow Down"
+                                width={15}
+                                height={15}
+                            />
+                        </span>
+                    </button>
+                    <button className=" text-[#F4450B] px-8 py-3 rounded-full flex items-center border-2 gap-2 cursor-pointer">
+                        Explore More
+                        <span>
+                            <img
+                                src={ArrowRightUp}
+                                alt="Arrow Down"
+                                width={15}
+                                height={15}
+                            />
+                        </span>
+                    </button>
+                </div>
+            </div>
+            {/* image */}
+            <div className="object-contain mr-5 md:mt-0 mt-24">
+                <img
+                    src={HeaderIMG}
+                    alt="Hero Image"
+                    width={550}
+                    height={550}
+                    className={styles.img}
+                />
+            </div>
         </div>
-      </div>
-      {/* image */}
-      <div className="object-contain mr-5 ">
-        <img
-          src={HeaderIMG}
-          alt="Hero Image"
-          width={550}
-          height={550}
-          className={styles.img}
-        />
-      </div>
-    </div>
-  );
+    );
 }
