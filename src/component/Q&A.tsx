fed775be301b3736804c391e423074d6de155f70
ryanfavour4 <ryanfave022@gmail.com>
2025-10-07 10:30:23 +0100

    Refactor FAQItem component for improved readability and consistency

--- src/component/Q&A.tsx
+++ src/component/Q&A.tsx
@@ -3,30 +3,30 @@ import ChevronUpIcon from "../../public/assets/chevron-up.svg";
 import ChevronDownIcon from "../../public/assets/chevron-down.svg";
 
 type FAQItemProps = {
-  question: string;
-  answer: string;
+    question: string;
+    answer: string;
 };
 
 export default function FaQItem({ question, answer }: FAQItemProps) {
-  const [isOpen, setIsOpen] = useState(false);
-  return (
-    <div>
-      <div className=" p-4">
-        <button
-          className="w-full flex-1 flex justify-between items-center text-left text-lg font-semibold text-gray-800"
-          onClick={() => setIsOpen(!isOpen)}
-        >
-          {question}
-          <span className="text-xl text-dark-purple w-9 h-9">
-            {isOpen ? (
-              <img src={ChevronUpIcon} />
-            ) : (
-              <img src={ChevronDownIcon} />
-            )}
-          </span>
-        </button>
-        {isOpen && <p className="text-gray-600 mt-2">{answer}</p>}
-      </div>
-    </div>
-  );
+    const [isOpen, setIsOpen] = useState(false);
+    return (
+        <div>
+            <div className=" p-4">
+                <button
+                    className="w-full justify-between  flex  items-center text-left text-lg font-semibold text-gray-800"
+                    onClick={() => setIsOpen(!isOpen)}
+                >
+                    {question}
+                    <span className=" w-5 h-5">
+                        {isOpen ? (
+                            <img src={ChevronUpIcon} />
+                        ) : (
+                            <img src={ChevronDownIcon} />
+                        )}
+                    </span>
+                </button>
+                {isOpen && <p className="text-gray-600 mt-2">{answer}</p>}
+            </div>
+        </div>
+    );
 }
