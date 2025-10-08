a7384bbe0006805e26900fb8aefdd05931bf2c44
jehovah-son <chukwukasuccess0@gmail.com>
2025-10-08 11:43:50 +0100

    `Refactor Hero component styles and layout`

--- src/component/Hero.tsx
+++ src/component/Hero.tsx
@@ -5,10 +5,10 @@ import ArrowRightUp from "../../public/Arrow right up.png";
 
 export default function Hero() {
   return (
-    <div className="flex md:flex-row flex-col items-center w-full gap-5 pt-20">
+    <div className="flex md:flex-row flex-col items-center md:px-0 px-3 w-full gap-5 md:pt-20 pt-10">
       {/* text */}
-      <div className="flex-col flex-1 md:ml-10 px-4">
-        <h1 className="text-5xl text-balance md:text-6xl font-bold mb-6 leading-[1.2]">
+      <div className="flex-col flex-1 md:ml-10">
+        <h1 className="md:text-6xl text-4xl font-bold mb-6 leading-[1.2]">
           Your Digital Bank for Every Moment.
         </h1>
         <p className=" max-w-[500px] text-justify text-base">
@@ -18,7 +18,7 @@ export default function Hero() {
           do — smarter, faster, and always secure.
         </p>
         {/* button */}
-        <div className="flex mt-8 gap-5">
+        <div className="md:flex-row flex-col flex w-fit  mt-8 gap-5">
           <button className="bg-[#F4450B]  text-white px-8 py-3 rounded-full flex items-center gap-2 cursor-pointer">
             Download
             <span>
@@ -28,13 +28,13 @@ export default function Hero() {
           <button className=" text-[#F4450B] px-8 py-3 rounded-full flex items-center border-2 gap-2 cursor-pointer">
             Explore More
             <span>
-              <img src={ArrowRightUp} alt="Arrow Down" width={15} height={15} />
+              <img src={ArrowRightUp} alt="Arrow Down" width={14} height={14} />
             </span>
           </button>
         </div>
       </div>
       {/* image */}
-      <div className="object-contain mr-5 md:mt-0 mt-24">
+      <div className="object-contain mr-5 md:pt-0 pt-7 ">
         <img
           src={HeaderIMG}
           alt="Hero Image"
